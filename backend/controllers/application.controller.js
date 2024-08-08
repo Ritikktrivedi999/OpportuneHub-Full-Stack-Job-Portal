657ad552bf8d25cc8245239f79a9dcd950f83b4f
Ritik Trivedi <ritikktrivedi9999@gmail.com>
2024-08-08 10:02:37 +0530

    Implemented Controller for apllication in which student can apply job, check there ststus. Admin can also update status , check applicants . Also implemented route for the same .

---
+++ backend/controllers/application.controller.js
@@ -0,0 +1,141 @@
+import { Application } from "../models/application.model.js";
+import { Job } from "../models/job.model.js";
+
+export const applyJob = async (req, res) => {
+    try {
+        const userId = req.id;
+        const jobId = req.params.id;
+        if (!jobId) {
+            return res.status(400).json({
+                message: "Job id is required.",
+                success: false
+            })
+        };
+        // check if the user already applied for the job or not
+        const existingApplication = await Application.findOne({ job: jobId, applicant: userId });
+
+        if (existingApplication) {
+            return res.status(400).json({
+                message: "You have already applied for this jobs",
+                success: false
+            });
+        }
+
+        // Job exist or Not
+        const job = await Job.findById(jobId);
+        if (!job) {
+            return res.status(404).json({
+                message: "Job not found",
+                success: false
+            })
+        }
+        //create a new applicstion so that he can apply
+        const newApplication = await Application.create({
+            job: jobId,
+            applicant: userId,
+        });
+
+        job.applications.push(newApplication._id);
+        await job.save();
+        return res.status(201).json({
+            message: "Job applied successfully.",
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+//get all applied jobs
+
+export const getAppliedJobs = async (req, res) => {
+    try {
+        const userId = req.id;
+        const application = await Application.find({ applicant: userId }).sort({ createdAt: -1 }).populate({
+            path: 'job',
+            options: { sort: { createdAt: -1 } },
+            populate: {
+                path: 'company',
+                options: { sort: { createdAt: -1 } },
+            }
+        });
+        if (!application) {
+            return res.status(404).json({
+                message: "No Applications",
+                success: false
+            })
+        };
+        return res.status(200).json({
+            application,
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+// Admin will check how many user applied 
+
+export const getApplicants = async (req, res) => {
+    try {
+        const jobId = req.params.id;
+        const job = await Job.findById(jobId).populate({
+            path: 'applications',
+            options: { sort: { createdAt: -1 } },
+            populate: {
+                path: 'applicant'
+            }
+        });
+        if (!job) {
+            return res.status(404).json({
+                message: 'Job not found.',
+                success: false
+            })
+        };
+        return res.status(200).json({
+            job,
+            succees: true
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// status that applicants is selected or rejected
+
+export const updateStatus = async (req, res) => {
+    try {
+        const { status } = req.body;
+        const applicationId = req.params.id;
+        if (!status) {
+            return res.status(400).json({
+                message: 'status is required',
+                success: false
+            })
+        };
+
+
+        //find application by applicant Id
+
+        const application = await Application.findOne({ _id: applicationId });
+        if (!application) {
+            return res.status(404).json({
+                message: "Application not found.",
+                success: false
+            })
+        };
+
+
+
+        //Update application status
+        application.status = status.toLowerCase();
+        await application.save();
+
+        return res.status(200).json({
+            message: "Status updated successfully.",
+            success: true
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+}
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
-import express from "express";
+import express, { application } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
-import jobRoute from "./routes/job.route.js"
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
 
 dotenv.config({});
 const app = express();
@@ -24,7 +25,8 @@ const PORT = process.env.PORT || 3000;
 //api connect
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/company", companyRoute);
-app.use("/api/v1/jobs",jobRoute)
+app.use("/api/v1/jobs", jobRoute)
+app.use("/api/v1/application", applicationRoute)
 app.listen(PORT, () => {
   console.log(`server is running at port ${PORT}`);
   connectDB();
