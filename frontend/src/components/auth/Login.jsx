e09734afa967082d4aa285e3997579946201f2d3
Ritik Trivedi <ritikktrivedi9999@gmail.com>
2024-08-09 14:56:16 +0530

    fixed minor bugs

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -19,6 +19,7 @@ const Login = () => {
         setInput({ ...input, [e.target.name]: e.target.value });
     }
 
+
     const submitHandler = async (e) => {
         e.preventDefault();
         console.log(input);
@@ -28,7 +29,7 @@ const Login = () => {
         <>
             <Navbar />
             <div className='flex items-center justify-center max-w-7xl mx-auto'>
-                <form className="w-1/2 border border-gray-200 rounded-md my-10 p-4" onClick={submitHandler}>
+                <form className="w-1/2 border border-gray-200 rounded-md my-10 p-4" onSubmit={submitHandler}>
                     <h1 className='font-bold text-xl mb-5'>Login</h1>
 
                     <div className='my-2'>
@@ -52,7 +53,7 @@ const Login = () => {
                             onChange={changeEventHandler}
                         />
                     </div>
-                    <div className="flex item-center justify-between">
+                    <div className='flex items-center justify-between'>
                         <RadioGroup className="flex items-center gap-4 my-5">
                             <div className="flex items-center space-x-2">
                                 <Input
@@ -79,7 +80,7 @@ const Login = () => {
                         </RadioGroup>
 
                     </div>
-                    <Button className="w-full my-4">Login</Button>
+                    <Button type="submit" className="w-full my-4">Login</Button>
                     <span className="text-sm">Dont`t have an account? <Link to="/signup" className="text-blue-600">Signup</Link></span>
                 </form>
             </div>
