43a0b5bfcf64e8a4fb3109adb3651ad8001b045c
Ritik Trivedi <ritikktrivedi9999@gmail.com>
2024-08-25 22:22:05 +0530

    Added Persister for store details

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,12 +1,42 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./authSlice"; 
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import authReducer from "./authSlice";
 import jobSlice from "./jobSlice";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import authSlice from "./authSlice";
+
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+}
+
+const rootReducer = combineReducers({
+    auth: authSlice,
+    job: jobSlice,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 
 const store = configureStore({
-    reducer: {
-        auth: authReducer, 
-        job:jobSlice
-    }
-})
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+});
 
 export default store;
