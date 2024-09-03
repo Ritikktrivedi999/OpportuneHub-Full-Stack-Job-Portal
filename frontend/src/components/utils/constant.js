d1afb8a94992a6d2bedf70d44073d814157a4885
Ritik Trivedi <ritikktrivedi9999@gmail.com>
2024-09-03 09:06:35 +0530

    "Updated API endpoint URLs from vercel.app to localhost:8000"

--- frontend/src/components/utils/constant.js
+++ frontend/src/components/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="https://opportune-hub-full-stack-job-portal-fvou.vercel.app//api/v1/users";
-export const JOB_API_END_POINT="https://opportune-hub-full-stack-job-portal-fvou.vercel.app//api/v1/jobs"
-export const APPLICATION_API_END_POINT="https://opportune-hub-full-stack-job-portal-fvou.vercel.app//api/v1/application"
-export const COMPANY_API_END_POINT="https://opportune-hub-full-stack-job-portal-fvou.vercel.app//api/v1/company"
+export const USER_API_END_POINT="http://localhost:8000/api/v1/users";
+export const JOB_API_END_POINT="http://localhost:8000/api/v1/jobs"
+export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application"
+export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company"
