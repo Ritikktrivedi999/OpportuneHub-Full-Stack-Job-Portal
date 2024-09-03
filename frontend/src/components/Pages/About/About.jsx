ca44b0d4a8520ba86c9ec1d1ef811162aacb1df6
Ritik Trivedi <ritikktrivedi9999@gmail.com>
2024-09-03 09:24:28 +0530

    image setup

--- frontend/src/components/Pages/About/About.jsx
+++ frontend/src/components/Pages/About/About.jsx
@@ -11,7 +11,7 @@ export default function About() {
                     <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
                         <div className="md:5/12 lg:w-5/12">
                             <img
-                                src="../../../../public/Screenshot (305).png"
+                                src="/Screenshot (305).png"
                                 alt="image"
                             />
                         </div>
