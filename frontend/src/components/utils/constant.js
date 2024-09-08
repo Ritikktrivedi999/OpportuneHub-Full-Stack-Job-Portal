f277f890d536e3cad22f62bd35de64d55d7b920f
Ritik Trivedi <ritikktrivedi9999@gmail.com>
2024-09-08 14:36:28 +0530

    Updated API Live

--- frontend/src/components/utils/constant.js
+++ frontend/src/components/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/users";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/jobs"
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application"
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company"
+export const USER_API_END_POINT="https://opportunehub-full-stack-job-portal.onrender.com/api/v1/users";
+export const JOB_API_END_POINT="https://opportunehub-full-stack-job-portal.onrender.com/api/v1/jobs"
+export const APPLICATION_API_END_POINT="https://opportunehub-full-stack-job-portal.onrender.com/api/v1/application"
+export const COMPANY_API_END_POINT="https://opportunehub-full-stack-job-portal.onrender.com/api/v1/company"
